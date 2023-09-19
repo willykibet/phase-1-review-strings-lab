763a9c7dbbbda4227158a38dc74aff1e7d651d5c
Willy <willykipchumba22@gmail.com>
2023-09-19 12:19:39 +0300

    a

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-const currentUser = 'Michelle June';
+const currentUser = 'willy kibet';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
